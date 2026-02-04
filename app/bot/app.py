c7d47859ea1c3f6fa9f7a83abe4a9fb9d08439ce
gregoryanvlad-cpu <gregoryan.vlad@gmail.com>
2026-02-04 10:35:03 +0300

    Update app.py

--- app/bot/app.py
+++ app/bot/app.py
@@ -6,6 +6,7 @@ from app.core.config import settings
 from app.bot.handlers.start import router as start_router
 from app.bot.handlers.nav import router as nav_router
 from app.bot.handlers.yandex import router as yandex_router
+from app.bot.handlers.referrals import router as referrals_router
 from app.bot.admin import router as admin_router
 from app.bot.middlewares import CorrelationIdMiddleware, RateLimitMiddleware
 
@@ -22,6 +23,7 @@ async def run_bot() -> None:
     dp.include_router(start_router)
     dp.include_router(nav_router)
     dp.include_router(yandex_router)
+    dp.include_router(referrals_router)
     dp.include_router(admin_router)
 
     log.info("bot_start")
