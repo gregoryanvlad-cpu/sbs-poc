35a9de31412c95b8319a414c14080da325718876
gregoryanvlad-cpu <gregoryan.vlad@gmail.com>
2026-02-06 00:25:39 +0300

    Update app.py

--- app/bot/app.py
+++ app/bot/app.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 from app.core.config import settings
 from app.bot.middlewares import CorrelationIdMiddleware
@@ -12,7 +13,7 @@ from app.bot.admin_kick import router as admin_kick_router
 def run_bot():
     bot = Bot(
         token=settings.bot_token,
-        parse_mode=ParseMode.HTML,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
 
     dp = Dispatcher()
