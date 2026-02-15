2e7c03630d22c14f38f8e989295450c82f6fe3eb
gregoryanvlad-cpu <gregoryan.vlad@gmail.com>
2026-02-15 20:54:30 +0300

    Update __init__.py

--- regionvpn/__init__.py
+++ regionvpn/__init__.py
@@ -1,1 +1,8 @@
+"""VPN-Region service (VLESS + Reality).
+
+The bot imports :class:`RegionVpnService` from this package.
+"""
+
 from .service import RegionVpnService
+
+__all__ = ["RegionVpnService"]
